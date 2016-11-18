b750fbc1d7716dd7fc3084a3d636d1033f3178f3
Adam Zinder <adam.zinder@gmail.com>
2016-11-17 20:02:07 -0800

    provide full skeleton structure for backend and frontend logic

--- app.rb
+++ app.rb
@@ -2,3 +2,12 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/word_counter')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get ('/output') do
+  @result = params.fetch('').word_count()
+  erb(:title)
+end
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -2,6 +2,15 @@ require('pry')
 require('rspec')
 require('word_counter')
 
+describe('the word count path', {:type => :feature}) do
+  it('processes the user entry and and returns the word count') do
+    visit('/')
+    fill_in('', :with => '')
+    click_button('')
+    expect(page).to have_content('')
+  end
+end
+
 describe('String#word_counter') do
   it "" do
     expect
